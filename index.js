612d6b6aec9c579347d18a7eeae09ae1bd9d555f
Anand Shrestha <anand.smartcode@gmail.com>
2020-08-30 16:13:55 +0545

    Set up backend

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+//import routes
+const authRoutes = require('./routes/auth');
+
+//app
+const app = express();
+
+
+// db
+mongoose
+    .connect(process.env.DATABASE,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB Connected'));
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+//routes middleware
+app.use('/api', authRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+});
